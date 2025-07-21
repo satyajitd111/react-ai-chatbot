5f39b89bf0e5cd5d004f9328697af6d6fcba504c
Satyajit <satyajit.das2@iqvia.com>
2025-07-21 18:04:06 +0530

    basic component and styling implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react';
+import { ChatbotIcon } from './components/ChatbotIcon';
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='chatbot-popup'>
+        {/* ChatBot Header */}
+        <div className='chat-header'>
+          <div className='header-info'>
+            <ChatbotIcon />
+            <h2 className='logo-text'>Chatbot</h2>
+          </div>
+          <button class='material-symbols-rounded'>keyboard_arrow_down</button>
+        </div>
+        {/* ChatBot Body */}
+        <div className='chat-body'>
+          <div className='message bot-message'>
+            <ChatbotIcon />
+            <p className='message-text'>
+              Hey there! <br /> How can I assist you today?
+            </p>
+          </div>
+          <div className='message user-message'>
+            <ChatbotIcon />
+            <p className='message-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        {/* ChatBot Footer */}
+        <div className='chat-footer'></div>
+        <form action='#' className='chat-form'>
+          <input
+            type='text'
+            placeholder='Message..'
+            className='message-input'
+            required
+          />
+          <button class='material-symbols-rounded'>arrow_upward</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
